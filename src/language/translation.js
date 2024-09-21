58dfe5bc2af35cad1e75e8a4b402c53ae774016f
yaxyobekb <pubgn9642@gmail.com>
2024-09-21 18:42:23 +0500

    add language provider

---
+++ src/language/translation.js
@@ -0,0 +1,12 @@
+const translations = {
+  en: {
+    welcome: "Welcome",
+    skills: "Skills",
+  },
+  uz: {
+    welcome: "Xush kelibsiz",
+    skills: "Ko'nikmalar",
+  },
+};
+
+export default translations;
--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,12 @@ import "./index.css";
 // Components
 import App from "./App.jsx";
 
+// Language provider
+import { LanguageProvider } from "./language/LanguageProvider.jsx";
+
 // Render the project
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <LanguageProvider>
+    <App />
+  </LanguageProvider>
+);
