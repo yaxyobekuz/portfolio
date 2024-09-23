b17ec509974c8fce3fbdf30c0fa09011fa591cd9
yaxyobekb <pubgn9642@gmail.com>
2024-09-23 12:03:47 +0500

    add user profile initial data

--- src/data/userProfile.js
+++ src/data/userProfile.js
@@ -1,31 +1,104 @@
+import gmailLogo from "../assets/images/icons/gmail-logo.svg";
+import githubLogo from "../assets/images/icons/github-logo.svg";
+import telegramLogo from "../assets/images/icons/telegram-logo.svg";
+import instagramLogo from "../assets/images/icons/instagram-logo.svg";
+import telephoneLogo from "../assets/images/icons/telephone-logo.svg";
+import addressLogo from "../assets/images/icons/address-location.svg";
+
+const instagramColor =
+  "radial-gradient(circle farthest-corner at 35% 90%, rgba(254, 197, 100, 0.25), transparent 50%),radial-gradient(circle farthest-corner at 0 140%, rgba(254, 197, 100, 0.25), transparent 50%),radial-gradient(ellipse farthest-corner at 0 -25%, rgba(82, 88, 207, 0.25), transparent 50%),radial-gradient(ellipse farthest-corner at 20% -50%, rgba(82, 88, 207, 0.25), transparent 50%),radial-gradient(ellipse farthest-corner at 100% 0, rgba(137, 61, 194, 0.25), transparent 50%),radial-gradient(ellipse farthest-corner at 60% -20%, rgba(137, 61, 194, 0.25), transparent 50%),radial-gradient(ellipse farthest-corner at 100% 100%, rgba(217, 49, 122, 0.25), transparent),linear-gradient(rgba(101, 89, 202, 0.25), rgba(188, 49, 143, 0.25) 30%, rgba(227, 63, 95, 0.25) 50%, rgba(247, 118, 56, 0.25) 70%, rgba(254, 198, 109, 0.25) 100%)";
+
 const userProfile = {
   firstName: "Yaxyobek",
   lastName: "Habibulloyev",
-  fullName: () => `${this.firstName} ${this.lastName}`,
   skills: ["JavaScript", "React", "CSS"],
+  fullName: () => `${userProfile.firstName} ${userProfile.lastName}`,
 
-  tags: [
-    { en: "Junior", uz: "Junior" },
-    { en: "Web Developer", uz: "Veb Dasturchi" },
-    { en: "Junior Designer", uz: "Yosh dizayner" },
-    { en: "N1 Bot", uz: "N1 Bot" },
-    { en: "Human", uz: "Odam" },
-  ],
+  tags: {
+    en: ["Junior", "Web Developer", "N1 Bot", "Junior Designer", "Human"],
+    uz: ["Junior", "Veb Dasturchi", "N1 Bot", "Yosh dizayner", "Odam"],
+  },
 
   socialLinks: {
-    github: "https://github.com/yaxyobekuz",
-    instagram: "https://www.instagram.com/yaxyobekh.uz",
-    telegram: "https://t.me/mryaxyobek",
+    en: [
+      {
+        name: "Github",
+        value: "https://github.com/yaxyobekuz",
+        icon: { url: githubLogo, fill: "#FFFFFF40" },
+      },
+      {
+        name: "Instagram",
+        value: "https://instagram.com/yaxyobekh.uz",
+        icon: { url: instagramLogo, fill: instagramColor },
+      },
+      {
+        name: "Telegram",
+        value: "https://t.me/mryaxyobek",
+        icon: { url: telegramLogo, fill: "#1B92D140" },
+      },
+    ],
+
+    uz: [
+      {
+        name: "Github",
+        value: "https://github.com/yaxyobekuz",
+        icon: { url: githubLogo, fill: "#FFFFFF40" },
+      },
+      {
+        name: "Instagram",
+        value: "https://instagram.com/yaxyobekh.uz",
+        icon: { url: instagramLogo, fill: instagramColor },
+      },
+      {
+        name: "Telegram",
+        value: "https://t.me/mryaxyobek",
+        icon: { url: telegramLogo, fill: "#1B92D140" },
+      },
+    ],
   },
 
   contactInfo: {
-    email: "pubgn9642@gmail.com",
-    phone: "+9982000077729",
+    en: [
+      {
+        type: "email",
+        name: "Email",
+        value: "pubgn9642@gmail.com",
+        icon: { url: gmailLogo, fill: "#ffffff" },
+      },
+      {
+        type: "tel",
+        name: "Phone number",
+        value: "+9982000077729",
+        icon: { url: telephoneLogo, fill: "#ffffff" },
+      },
+      {
+        type: "address",
+        name: "Address",
+        value: "Uzbekistan, Andijan",
+        icon: { url: addressLogo, fill: "#ffffff" },
+      },
+    ],
 
-    location: {
-      en: "Uzbekistan, Andijan",
-      uz: "O'zbekiston, Andijon",
-    },
+    en: [
+      {
+        type: "email",
+        name: "E-pochta",
+        value: "pubgn9642@gmail.com",
+        icon: { url: gmailLogo, fill: "#ffffff" },
+      },
+      {
+        type: "tel",
+        name: "Telefon raqam",
+        value: "+9982000077729",
+        icon: { url: telephoneLogo, fill: "#ffffff" },
+      },
+      {
+        type: "address",
+        name: "Manzil",
+        value: "Uzbekistan, Andijan",
+        icon: { url: addressLogo, fill: "#ffffff" },
+      },
+    ],
   },
 };
 
