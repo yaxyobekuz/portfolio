61cbcc938cbcdf942753b397d863a78e42413599
yaxyobekb <pubgn9642@gmail.com>
2024-09-28 16:52:21 +0500

    add site focus styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="flex flex-col items-start justify-end min-h-full pt-12 pb-20 sm:py-96 sm:items-center sm:justify-center">
+    <div className="flex flex-col items-start justify-end min-h-full pt-12 pb-20 sm:py-24 sm:items-center sm:justify-center">
       <div className="flex flex-col items-start gap-6 max-w-96 sm:items-center sm:max-w-xl md:max-w-2xl">
         {/* Title */}
         <h1 className="markdown strong-primary text-3xl font-semibold sm:text-center md:leading-normal md:text-4xl">
