23266a1d02af2f0119ef3e5686d73d677bcc6239
yaxyobekb <pubgn9642@gmail.com>
2024-09-28 16:51:56 +0500

    add sidebar responsive styles

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,25 +1,38 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 
 // Components
 import Header from "../components/Header";
+import NavBar from "../components/NavBar";
+import Sidebar from "../components/Sidebar";
 
 // Images
 import backgroundImage from "../assets/images/backgrounds/body.jpg";
-import Sidebar from "../components/Sidebar";
 
 const MainLayout = () => {
+  const location = useLocation();
+  const [isOpenSidebar, setIsOpenSidebar] = useState(false);
+  useEffect(() => {
+    setIsOpenSidebar(false);
+  }, [location]);
+
   return (
     <>
       {/* --- Content --- */}
       <div className="h-screen relative z-1">
         {/* Header */}
-        <Header />
+        <Header
+          isOpenSidebar={isOpenSidebar}
+          toggleOpenSidebar={() => setIsOpenSidebar((state) => !state)}
+        />
 
         {/* Main */}
-        <main className="flex h-[calc(100%-56px)]">
+        <main className="flex h-[calc(100%-112px)] lg:h-[calc(100%-56px)]">
           {/* sidebar */}
-          <Sidebar />
+          <Sidebar
+            isOpen={isOpenSidebar}
+            onClose={() => setIsOpenSidebar(false)}
+          />
 
           {/* pages */}
           <div className="w-full h-full overflow-y-auto scroll-primary sm:scroll-primary-large">
@@ -28,6 +41,8 @@ const MainLayout = () => {
             </div>
           </div>
         </main>
+
+        <NavBar />
       </div>
 
       {/* --- Background --- */}
