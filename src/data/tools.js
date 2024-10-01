e2f55956a875a8a309dd8c0a9c41ce8b94149358
yaxyobekb <pubgn9642@gmail.com>
2024-10-01 16:58:49 +0500

    add tools data

---
+++ src/data/tools.js
@@ -0,0 +1,43 @@
+import jsLogo from "../assets/images/tools/js.svg";
+import cssLogo from "../assets/images/tools/css.svg";
+import htmlLogo from "../assets/images/tools/html.svg";
+import antdLogo from "../assets/images/tools/antd.svg";
+import figmaLogo from "../assets/images/tools/figma.svg";
+import reactLogo from "../assets/images/tools/react.svg";
+import swiperLogo from "../assets/images/tools/swiper.svg";
+import tailwindcssLogo from "../assets/images/tools/tailwindcss.svg";
+
+export default [
+  {
+    name: "HTML",
+    icon: htmlLogo,
+  },
+  {
+    name: "CSS",
+    icon: cssLogo,
+  },
+  {
+    name: "JS",
+    icon: jsLogo,
+  },
+  {
+    name: "React JS",
+    icon: reactLogo,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwindcssLogo,
+  },
+  {
+    name: "Figma",
+    icon: figmaLogo,
+  },
+  {
+    name: "Swiper JS",
+    icon: swiperLogo,
+  },
+  {
+    name: "ANTD",
+    icon: antdLogo,
+  },
+];
