860d558e6e7810963b4388e7a9908bca4cca2e70
yaxyobekb <pubgn9642@gmail.com>
2024-10-01 18:27:50 +0500

    add advantages data

---
+++ src/data/advantages.js
@@ -0,0 +1,52 @@
+import webIcon from "../assets/images/advantages/web.svg";
+import penIcon from "../assets/images/advantages/pen.svg";
+import timeIcon from "../assets/images/advantages/time.svg";
+import diamondIcon from "../assets/images/advantages/diamond.svg";
+
+export default {
+    uz: [
+      {
+        title: "SEO optimizatsiya",
+        description: "Saytni qidiruv tizimlarida yuqori o'rinlarga ko'tarish",
+        icon: { url: webIcon, fill: "rgba(92, 176, 255, 0.25)" },
+      },
+      {
+        title: "Kuchli dizayn",
+        description: "Dizayn jarayonida nafislik va detallarga katta e’tibor qaratish",
+        icon: { url: penIcon, fill: "rgba(255, 80, 125, 0.25)" },
+      },
+      {
+        title: "Sifatli ishlab chiqish",
+        description: "Saytlarni yuqori sifatli va zamonaviy standartlarga mos ishlab chiqish",
+        icon: { url: diamondIcon, fill: "rgba(165, 234, 214, 0.25)" },
+      },
+      {
+        title: "Tezkor ish jarayoni",
+        description: "Belgilangan vaqt ichida samarali va tezkor sayt yaratish",
+        icon: { url: timeIcon, fill: "rgba(0, 238, 122, 0.25)" },
+      },
+    ],
+    en: [
+      {
+        title: "SEO Optimization",
+        description: "Boost website ranking in search engine results",
+        icon: { url: webIcon, fill: "rgba(92, 176, 255, 0.25)" },
+      },
+      {
+        title: "Strong Design",
+        description: "Focus on elegance and attention to small details in design",
+        icon: { url: penIcon, fill: "rgba(255, 80, 125, 0.25)" },
+      },
+      {
+        title: "High-Quality Development",
+        description: "Develop websites with top-notch quality and modern standards",
+        icon: { url: timeIcon, fill: "rgba(165, 234, 214, 0.25)" },
+      },
+      {
+        title: "Efficiency",
+        description: "Create websites quickly and efficiently within short timeframes",
+        icon: { url: diamondIcon, fill: "rgba(0, 238, 122, 0.25)" },
+      },
+    ],
+  };
+  
--- src/i18n.js
+++ src/i18n.js
@@ -5,9 +5,10 @@ import { initReactI18next } from "react-i18next";
 // data
 import en from "./locales/en.json";
 import uz from "./locales/uz.json";
+import advantages from "./data/advantages";
 import userProfile from "./data/userProfile";
 
-const savedLang = localStorage.getItem('language');
+const savedLang = localStorage.getItem("language");
 const currentLanguage = savedLang ? savedLang : "en";
 const { tags, socialLinks, contactInfo } = userProfile;
 
@@ -25,12 +26,14 @@ i18n.use(initReactI18next).init({
 
 i18n.addResources("en", "translation", {
   tags: tags.en,
+  advantages: advantages.en,
   socialLinks: socialLinks.en,
   contactInfo: contactInfo.en,
 });
 
 i18n.addResources("uz", "translation", {
   tags: tags.uz,
+  advantages: advantages.uz,
   socialLinks: socialLinks.uz,
   contactInfo: contactInfo.uz,
 });
